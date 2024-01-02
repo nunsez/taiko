31586249873b08e2b7b863cb35c9f0c1cf1ce4b1
Alexander Mandrikov <mandrikov@pm.me>
2024-01-02 15:34:31 +0700

    configure test env db url

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :taiko, Taiko.Repo,
-  url: "postgresql://postgres:postgres@db:5432/taiko_test#{System.get_env("MIX_TEST_PARTITION")}",
+  url: System.get_env("DATABASE_URL", "postgresql://postgres:postgres@db:5432/taiko_test#{System.get_env("MIX_TEST_PARTITION")}"),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
