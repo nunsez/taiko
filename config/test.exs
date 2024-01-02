6819288731026ae2632093ca786b815b569fe712
Alexander Mandrikov <mandrikov@pm.me>
2024-01-02 15:16:07 +0700

    update test db url

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :taiko, Taiko.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "taiko_test#{System.get_env("MIX_TEST_PARTITION")}",
+  url: "postgresql://postgres:postgres@db:5432/taiko_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
