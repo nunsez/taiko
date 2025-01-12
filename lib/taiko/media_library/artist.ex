42012965d6ff52078f89aa73863a3f8c9946910f
Alexander Mandrikov <mandrikov@pm.me>
2025-01-12 18:43:05 +0700

    create artists

---
+++ lib/taiko/media_library/artist.ex
@@ -0,0 +1,26 @@
+defmodule Taiko.MediaLibrary.Artist do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Taiko.MediaLibrary.Song
+  alias Taiko.MediaLibrary.SongArtist
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "artists" do
+    field :name, :string
+
+    has_many :song_artists, SongArtist, on_delete: :delete_all
+
+    many_to_many :songs, Song, join_through: SongArtist
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(artist, attrs) do
+    artist
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20241230060200_create_artists.exs
@@ -0,0 +1,14 @@
+defmodule Taiko.Repo.Migrations.CreateArtists do
+  use Ecto.Migration
+
+  def change do
+    create table(:artists, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:artists, [:name])
+  end
+end
